0eaee20ab120b96029c0f9f750c147799795ac80
Ivan Pershin <shekko-777@yandex.ru>
2023-04-12 17:36:17 +0300

    feat: Добавить возможность ставить лайки

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// Поставить лайк
+let likeBtn = document.querySelectorAll(".elements__like");
+
+for (like of likeBtn) {
+  let itemElem = like.closest(".elements__item");
+  let likeBtn = itemElem.querySelector(".elements__like");
+
+  likeBtn.addEventListener("click", function () {
+    likeBtn.classList.toggle("elements__like_active");
+  });
+}
