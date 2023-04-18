7b4156b5e8e822eb6408b0ebd393816aac0b6448
Ivan Pershin <shekko-777@yandex.ru>
2023-04-18 14:43:17 +0300

    refactor: Исправить ошибки по код-ревью

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,37 @@
-let popup = document.querySelector(".popup"); //Сам popup
-let likeButtons = document.querySelectorAll(".elements__like"); //Сердечки на карточках
-let formInputs = document.querySelectorAll(".popup__input"); //Инпуты формы
-let formSaveSubmit = document.querySelector(".popup__save-btn"); //Кнопка сохранения формы
-let profileName = document.querySelector(".profile__title"); //Имя в профиле
-let profileJob = document.querySelector(".profile__subtitle"); //Профессия в профиле
-let profileEditor = document.querySelector(".profile__edit-btn"); //Кнопка изменения профиля
-let images = document.querySelectorAll(".elements__img"); //Картинки
-
-// Автозаполнение alt у картинок
-function handleAltImages() {
-  images.forEach((el) => {
-    let imagesItem = el.closest(".elements__item");
-    let imagesTitle = imagesItem.querySelector(".elements__title");
-    el.setAttribute("alt", imagesTitle.textContent);
-  });
-}
-handleAltImages();
-
-// Поставить лайк
-likeButtons.forEach((el) => {
-  el.addEventListener("click", () => {
-    el.classList.toggle("elements__like_active");
-  });
-});
+const popup = document.querySelector(".popup"); //Сам popup
+const likeButtons = document.querySelectorAll(".elements__like"); //Сердечки на карточках
+const formInputs = document.querySelectorAll(".popup__input"); //Инпуты формы
+const formSaveSubmit = document.querySelector(".popup__save-btn"); //Кнопка сохранения формы
+const profileName = document.querySelector(".profile__title"); //Имя в профиле
+const profileJob = document.querySelector(".profile__subtitle"); //Профессия в профиле
+const profileEditor = document.querySelector(".profile__edit-btn"); //Кнопка изменения профиля
+const images = document.querySelectorAll(".elements__img"); //Картинки
+const inputValueName = document.querySelector(".popup__input_place_title"); //Имя профиля
+const inputValueJob = document.querySelector(".popup__input_place_subtitle"); //Профессия
+const profileEditorClose = document.querySelector(".popup__close"); //Крестик закрытия редактирования
 
 // Функция изменения профиля
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let inputValueName = formInputs[0].value.length;
-  let inputValueJob = formInputs[1].value.length;
-
-  // Проверка на колличество символом. Ради тест-эксперимента :)
-  // "Горячий-шоколад" бери, дальше без него никак (─‿‿─)
-  if (inputValueName <= 3 || inputValueJob <= 3) {
-    console.log("Маленькое значение, введите больше");
-  } else if (inputValueName >= 40 || inputValueJob >= 40) {
-    console.log("Чет как-то много написано :/");
-  } else {
-    profileName.textContent = formInputs[0].value;
-    profileJob.textContent = formInputs[1].value;
-    popup.classList.remove("popup_opened"); //Закроет редактирование при сохранении
-  }
+  profileName.textContent = inputValueName.value;
+  profileJob.textContent = inputValueJob.value;
+  popup.classList.remove("popup_opened"); //Закроет редактирование при сохранении
 }
 
 formSaveSubmit.addEventListener("click", handleFormSubmit);
 
-// Открытие и закрытие профиля
-profileEditor.addEventListener("click", () => {
-  let profileEditorClose = document.querySelector(".popup__close");
+// Открытие профиля
+function openPopup() {
   popup.classList.add("popup_opened");
+  inputValueName.value = profileName.textContent;
+  inputValueJob.value = profileJob.textContent;
+}
 
-  formInputs[0].value = profileName.textContent;
-  formInputs[1].value = profileJob.textContent;
+profileEditor.addEventListener("click", openPopup);
 
-  // Закрытие редактирования профиля
-  profileEditorClose.addEventListener("click", () => {
-    popup.classList.remove("popup_opened");
-  });
-  // Закрытие редактирования при нажатии ESC
-  document.addEventListener("keydown", (evt) => {
-    let key = evt.key;
-    if (key === "Escape" && popup.classList.contains("popup_opened")) {
-      popup.classList.remove("popup_opened");
-      console.log("esc");
-    }
-  });
-});
+// Закрытие редактирования профиля
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+profileEditorClose.addEventListener("click", closePopup);
