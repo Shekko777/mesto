ad5bd231928336b6d647aa0305b349d4192eb330
Ivan Pershin <shekko-777@yandex.ru>
2023-04-29 15:05:22 +0300

    feat: Добавить открытие popup-add

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
-const popup = document.querySelector(".popup"); //Сам popup
 const formProfile = document.querySelector(".popup__form"); //Форма в popup
 const formInputs = document.querySelectorAll(".popup__input"); //Инпуты формы
 const profileName = document.querySelector(".profile__title"); //Имя в профиле
 const profileJob = document.querySelector(".profile__subtitle"); //Профессия в профиле
-const profileEditor = document.querySelector(".profile__edit-btn"); //Кнопка изменения профиля
 const images = document.querySelectorAll(".elements__img"); //Картинки
 const inputValueName = document.querySelector(".popup__input_place_title"); //Имя профиля
 const inputValueJob = document.querySelector(".popup__input_place_subtitle"); //Профессия
-const profileEditorClose = document.querySelector(".popup__close"); //Крестик закрытия редактирования
 const listCards = document.querySelector(".elements__list"); // Список карточек
 const templateCards = document.querySelector(".template-cards").content; // Template для клонирования содержимого
+const popupEdit = document.querySelector(".popup_type_edit"); // Popup изменения профиля
+const popupAdd = document.querySelector(".popup_type_add"); // Popup добавления новой карточки
+const profileEditor = document.querySelector(".profile__edit-btn"); //Кнопка изменения профиля
+const profileAdd = document.querySelector(".profile__add-btn"); // Кнопка добавления карточки
+
 const initialCards = [
   {
     name: "Архыз",
@@ -57,25 +59,25 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = inputValueName.value;
   profileJob.textContent = inputValueJob.value;
-  popup.classList.remove("popup_opened"); //Закроет редактирование при сохранении
+  popupEdit.classList.remove("popup_opened"); //Закроет редактирование при сохранении
 }
 
 formProfile.addEventListener("submit", handleFormSubmit);
 
-// Открытие профиля
-function openPopup() {
-  popup.classList.add("popup_opened");
+// Функция открытие профиля
+function openPopup(popupName) {
+  popupName.classList.add("popup_opened");
   inputValueName.value = profileName.textContent;
   inputValueJob.value = profileJob.textContent;
+
+  // Закрытие открытого popup
+  popupName
+    .closest(".popup")
+    .querySelector(".popup__close")
+    .addEventListener("click", function () {
+      popupName.classList.remove("popup_opened");
+    });
 }
 
-//"Плитка шоколада" для буста skill's
-
-profileEditor.addEventListener("click", openPopup);
-
-// Закрытие редактирования профиля
-function closePopup() {
-  popup.classList.remove("popup_opened");
-}
-
-profileEditorClose.addEventListener("click", closePopup);
+profileEditor.addEventListener("click", () => openPopup(popupEdit)); // Открытие редактирование профиля
+profileAdd.addEventListener("click", () => openPopup(popupAdd)); // Открытие добавления карточки
