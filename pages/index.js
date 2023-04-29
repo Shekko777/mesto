b7a48a17fc89387e78190194e71b70d79a1135de
Ivan Pershin <shekko-777@yandex.ru>
2023-04-29 11:28:57 +0300

    feat: Добавить функцию 'Карточки из коробки'

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,49 @@ const images = document.querySelectorAll(".elements__img"); //Картинки
 const inputValueName = document.querySelector(".popup__input_place_title"); //Имя профиля
 const inputValueJob = document.querySelector(".popup__input_place_subtitle"); //Профессия
 const profileEditorClose = document.querySelector(".popup__close"); //Крестик закрытия редактирования
+const listCards = document.querySelector(".elements__list"); // Список карточек
+const templateCards = document.querySelector(".template-cards").content; // Template для клонирования содержимого
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+]; // список карточек
+
+// Функция добавления карточки
+function addNewCards(image, title) {
+  const liCards = templateCards
+    .querySelector(".elements__item")
+    .cloneNode(true);
+  liCards.querySelector(".elements__title").textContent = title;
+  liCards.querySelector(".elements__img").src = image;
+  liCards.querySelector(".elements__img").alt = `Фотокарточка ${title}`;
+  listCards.append(liCards);
+}
+
+initialCards.forEach((card) => {
+  addNewCards(card.link, card.name);
+});
 
 // Функция изменения профиля
 function handleFormSubmit(evt) {
