5946671aeeeb06c838f38bfe6ecc75c86cc6f203
Ivan Pershin <shekko-777@yandex.ru>
2023-09-19 21:49:18 +0300

    feat: Добавить карточки с сервера, инфо юзера, удаление карточек

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,8 +1,4 @@
-const images = document.querySelectorAll(".elements__img"); //Картинки
 const templateCard = document.querySelector(".template-card").content; // Template для клонирования содержимого
-const cardListContainer = document.querySelector(".elements__list"); // Список карточек
-const buttonCloseElement = document.querySelectorAll(".popup__close"); // Крестик закрытия попапов
-const popupForms = document.querySelector(".popup__form");
 
 // Popup изменения профиля
 const inputValueName = document.querySelector(".popup__input_place_title"); //Имя профиля
@@ -10,20 +6,15 @@ const inputValueJob = document.querySelector(".popup__input_place_subtitle"); //
 const profileName = document.querySelector(".profile__title"); //Имя в профиле
 const profileJob = document.querySelector(".profile__subtitle"); //Профессия в профиле
 const formProfile = document.querySelector(".popup__form_type_edit"); //Форма в popup-edit
-const popupEdit = document.querySelector(".popup_type_edit"); // Popup изменения профиля
 const profileEditor = document.querySelector(".profile__edit-btn"); //Кнопка изменения профиля
 
 // Popup добавления карточки
 const popupAdd = document.querySelector(".popup_type_add"); // Popup добавления новой карточки
 const profileAdd = document.querySelector(".profile__add-btn"); // Кнопка добавления карточки
-const popupAddName = document.querySelector(".popup__input_add_name"); // Инпут имени карточки
-const popupAddLink = document.querySelector(".popup__input_add_link"); // Инпут ссылки карточки
 const popupAddForm = document.querySelector(".popup__form_type_add"); // Форма добавления карточки
 
-// Popup картинки
-const popupImage = document.querySelector(".popup-images"); // Попап картинок
-const imageCard = document.querySelector(".popup-images__img"); // Картинка
-const figcaptionCard = document.querySelector(".popup-images__figcaption"); // Описание картинки
+// Попап подтверждения
+const popupFormConfirm = document.querySelector(".popup__form_type_delete");
 
 // Обьект формы
 const formObject = {
@@ -35,35 +26,28 @@ const formObject = {
   errorClass: "popup__error_visible",
 };
 
+// Конфигурация API
 const apiConfig = {
-  url: "https://mesto.nomoreparties.co/v1/cohort-73",
+  url: "https://mesto.nomoreparties.co/v1/cohort-75",
   headers: {
     "Content-Type": "application/json",
-    authorization: "4e96e45f-913a-4a1f-b36d-22c36d55bd2e",
+    authorization: "a4c45c7f-c893-4f18-9f88-b5a255df5416",
   },
 };
 
+// Экспорты
 export {
-  images,
   templateCard,
-  cardListContainer,
-  buttonCloseElement,
-  popupForms,
   inputValueJob,
   inputValueName,
   profileName,
   profileAdd,
   profileJob,
   formProfile,
-  popupEdit,
   profileEditor,
   popupAdd,
-  popupAddName,
-  popupAddLink,
   popupAddForm,
-  popupImage,
-  imageCard,
-  figcaptionCard,
   formObject,
   apiConfig,
+  popupFormConfirm,
 };
